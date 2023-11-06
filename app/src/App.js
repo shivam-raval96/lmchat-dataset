8ee03d2de4f179d97cbec08ac04aa20703cd2985
Shivam-raval96 <sraval@fas.harvard.edu>
2023-11-06 20:34:07 +1100

    added explorable viz

--- app/src/App.js
+++ app/src/App.js
@@ -6,41 +6,58 @@ import IconButton from '@mui/material/IconButton';
 import CircularProgress from '@mui/material/CircularProgress';
 
 import Scatterplot  from './plotdata'
-import frankenstein from './datasets/frankenstein.json'
-import frankenstein_labels from './datasets/frankenstein_labels.json'
-
-import frankenstein_time from './datasets/frankenstein_time.json'
-import frankenstein_time_labels from './datasets/frankenstein_time_labels.json'
-
-import frankenstein_emotions from './datasets/frankenstein_emotions.json'
-import frankenstein_emotions_labels from './datasets/frankenstein_emotions_labels.json'
-
-import frankenstein_characters from './datasets/frankenstein_characters.json'
-import frankenstein_characters_labels from './datasets/frankenstein_characters_labels.json'
-
+import small_conversation from './datasets/small_conversation.json'
+import small_conversation_l20 from './datasets/small_conversation_l20.json'
+import small_conversation_l25 from './datasets/small_conversation_l25.json'
+import small_conversation_l35 from './datasets/small_conversation_l35.json'
+import small_conversation_l39 from './datasets/small_conversation_l39.json'
 
 const localDevURL = "http://127.0.0.1:8000/";
 axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
-let data_all ={"baseline":frankenstein,"time":frankenstein_time, "emotions":frankenstein_emotions, "characters":frankenstein_characters }
-let labels_all ={"baseline":frankenstein_labels,"time":frankenstein_time_labels, "emotions":frankenstein_emotions_labels, "characters":frankenstein_characters_labels}
+let data_all ={'chats':small_conversation, 'l20':small_conversation_l20,'l25':small_conversation_l25,
+'l35':small_conversation_l35,'l39':small_conversation_l39}
+let labels_all ={'chats':small_conversation}
 
 function App() {
 
     
   
-  const [plottedData, setPlottedData] = useState(frankenstein);
-  const [labelData, setLabelData] = useState(frankenstein_labels);
+  const [plottedData, setPlottedData] = useState(small_conversation);
+  const [labelData, setLabelData] = useState(small_conversation);
   const [loading, setLoading] = useState(false);
+  const [colorcol, setColorCol] = useState("5");
+  const [chatcontent, setChatContent] = useState("");
+  const [searched, setSearched] = useState([]);
 
-  const [theme, setTheme] = useState('emotions');
+
+  const [clusterBy, setClusterBy] = useState('chats');
 
 
-  const handleSend = () => {
-    console.log(data_all["time"])
-    setPlottedData(data_all[theme])
-    setLabelData(labels_all[theme])
+  function searchStringInArray (str, strArray) {
+    let indices = []
+    for (var j=0; j<strArray.length; j++) {
+        if (strArray[j][2].toLowerCase().match(str)) indices.push(j);
+        
+    }
+    if (indices.length>0)return indices;
+    return [];
+}
+  
+
+function handleSearch(){
+  setSearched([])
+    if (document.getElementById("search").value){
+      let found = searchStringInArray (document.getElementById("search").value.toLowerCase(), plottedData)
+      setSearched(found)
+      console.log(found)
+    }
+
+  }
+
+  function handleChagesearch(){
+    handleSearch()
 
   }
 
@@ -60,42 +77,9 @@ function App() {
       }
   };
 
-  const handleDownload = () => {
-      if (plottedData) {
-          const blob = new Blob([JSON.stringify(plottedData, null, 2)], { type: 'application/json' });
-          const link = document.createElement('a');
-          link.href = URL.createObjectURL(blob);
-          link.download = 'projections_'+theme+'.json';
-          document.body.appendChild(link);
-          link.click();
-          document.body.removeChild(link);
-      } else {
-          alert('No JSON data to download');
-      }
-  };
 
 
-  /*const handleSend = () => {
-    setLoading(true)
-    let req = {
-        data: selectedOption,
-        theme: theme,
-      };
-    
-    axios
-    .post(localDevURL + "modify-embeddings", req)
-    .then((response) => {
-      console.log(response.data.data)
-      setPlottedData(frankenstein_emotions)
-      setLabelData(frankenstein_emotions_labels)
-      setLoading(false)
-    })
-    .catch((error) => {
-      console.log(error);
-      setLoading(false)
-    });
-    
-  };*/
+
 
 
 
@@ -105,48 +89,52 @@ function App() {
       <div style={{ position: 'fixed', top: 20, left: 20, width: '300px', backgroundColor: 'rgba(0, 0, 0, 0.1)',
                  boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.5)',  // Drop shadow
                 borderRadius: '20px' ,                         // Curved edges
-                fontFamily: 'Perpetua'  // Setting the font family
+                fontFamily: 'Perpetua',  // Setting the font family
+                padding: '10px'
 
             }}>
 
         
-        <h2>Embedding Projection Modifier</h2>
+        <h2>lmChat Explorer</h2>
 
        Dataset: <select 
-        value={'Frankenstein'} 
+        value={'chats'} 
         onChange={null}
-        style={{ width: '40%', padding: '5px', borderRadius: '5px' }}>
-        <option value="frankstein">Frankenstein</option>
+        style={{ width: '50%', padding: '5px', borderRadius: '5px' }}>
+        <option value="chats">Small Conv</option>
       </select>
+      
+      <br/><br/>
 
-      <button  style={{ margin:"15px", padding: '5px'}}onClick={handleDownload}>Save View</button>
-      Cluster by: &nbsp;<input 
-            style={{display:'none'}}
-            type="text"
-            value={theme}
-            onChange={e => {setTheme(e.target.value)}}
-         /> 
+      Colored by: &nbsp;<select 
+        value={colorcol} 
+        onChange={e => {setColorCol(e.target.value)}}
+        style={{ width: '50%', padding: '5px', borderRadius: '5px' }}>
+        <option value="4">Parent label</option>
+        <option value="5">Child label</option>
+
+      </select>
+      
+      <br/><br/>
+      Projected based on: &nbsp;
          <select 
-        value={theme} 
-        onChange={e => {setTheme(e.target.value)}}
-        style={{ width: '40%', padding: '5px', borderRadius: '5px' }}>
-        <option value="emotions">Emotions</option>
-        <option value="time">Time of day</option>
-        <option value="characters">Characters</option>
+        value={clusterBy} 
+        onChange={e => {setClusterBy(e.target.value)}}
+        style={{ width: '50%', padding: '5px', borderRadius: '5px' }}>
+        <option value="chats">Content Similarity</option>
+        <option value="l20">L20 Activations</option>
+        <option value="l25">L25 Activations</option>
+        <option value="l35">L35 Activations</option>
+        <option value="l39">L39 Activations</option>
 
       </select>
-         <IconButton aria-label="send">
 
-            {(loading)?<CircularProgress size="1.5rem"  color="inherit"style={{}}/>:<SendIcon onClick={handleSend}/>}
-         
-       </IconButton>
+      
 
 
-        <p style={{ paddingLeft: "15px",paddingRight: "15px", display:'none'}} align="left" >Each point is an embedded text. Visual clusters are identified by a clustering algorithm. <br /><br />
-      This clustering may not be optimal for your task. You can change this!<br /><br />
-        This view may not reflect the actual clustering in high dimensions. 
-        
-        </p>
+
+       
+
         
         <div style={{display:'none'}}>
         <label id="name"><h4 style={{ paddingLeft: "15px",paddingTop: "0px"}} align="left">Load Projection</h4> </label>
@@ -156,8 +144,29 @@ function App() {
    
         </div>
 
+        <div style={{ position: 'fixed', top: 20, left: '78%', width: '300px', height: '295px', backgroundColor: 'rgba(0, 0, 0, 0.1)',
+                 boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.5)',  // Drop shadow
+                borderRadius: '20px' ,                         // Curved edges
+                fontFamily: 'Perpetua',  // Setting the font family
+                padding: '10px',
+                overflow:'scroll',
+                
+
+            }}>
+            <b>Chat Content</b>
+
+            <div id = "chatcontent" style={{ padding:'10px'}} ></div>
+        </div>
+
         <div>
-            <Scatterplot data={plottedData} labels ={labelData} width={1200} height={800} />
+            <Scatterplot data={data_all[clusterBy]} labels ={labelData} colorcol={colorcol} width={1000} height={800} />
+        </div>
+
+
+
+
+        <div style={{ position: 'fixed', top: "96.5%", left: '86%', }}>
+            @ Insight and Interaction Lab
         </div>
     </div>
   );
--- app/src/plotdata.js
+++ app/src/plotdata.js
@@ -13,15 +13,27 @@ const colors = [
 ]
 
 const colors2 = [
-    "#17becf", "#bcbd22", "#7f7f7f", "#e377c2", "#8c564b", 
+    "#17becf", "#17becf", "#7f7f7f", "#e377c2", "#e377c2", 
     "#9467bd", "#d62728", "#2ca02c", "#ff7f0e", "#1f77b4"
 ]
 
+var legend_dict = {
+    5:{male: "#17becf", female: "#17becf", upper: "#ff7f0e", middle:"#e377c2", lower: "#2ca02c"},
+    4:{gender: "#2ca02c", socioeco:"#d62728"}
+}
+
+
+
+
 
 function Scatterplot(props) {
-  const { width, height, data , labels} = props;
-  const margin = {top:50, left:80, right:80, bottom:10}
+  const { width, height, data , labels, colorcol} = props;
+  const margin = {top:50, left:80, right:80, bottom:20}
   const ref = useRef();
+  const legendref = useRef();
+
+
+  const color_dict = legend_dict[+colorcol]
 
   useEffect(() => {
     const svg = d3.select(ref.current);
@@ -43,13 +55,13 @@ function Scatterplot(props) {
     const tooltip = d3.select("body").append("div")
         .attr("class", "tooltip")
         .style("opacity", 0)
-        .style("background-color", "white")
-        .style("border", "solid")
-        .style("border-width", "2px")
-        .style("border-radius", "5px")
-        .style("padding", "5px")
-        .style("margin-right", "50px")
+        .style("margin-right", "300px")
         .style("position", "absolute");
+
+    d3.select("#chatcontent").selectAll('*').remove()
+
+    // Chat content
+    const chatcontent = d3.select("#chatcontent").append("div")
 
 
     // Bind data to circles and add tooltips
@@ -60,27 +72,73 @@ function Scatterplot(props) {
         .attr('cx', d => xScale(d[0]))
         .attr('cy', d => yScale(d[1]))
         .attr('r', 4)
-        .style("opacity", d =>  d[3]===-1? 0.2 : 0.9)
-        .attr('fill', d =>  d[3]===-1?'grey':colors[d[3]%20])
+        .style("opacity", d =>  d[3]===-1? 0.2 : 0.7)
+        .attr('fill', d => color_dict[d[+colorcol]])
         .attr('stroke', 'black')  // Add this line for the boundary color
         .attr('stroke-width', 0.5)  // Add this line for the boundary width
         .on("mouseover", (event, d) => {
+            svg.selectAll('circle')
+            .transition().duration(100)
+            .style("opacity", d =>  d[3]===-1? 0.2 : 0.7) 
+            .attr('stroke-width', 0.5) 
+            .attr('r', 4);
+            // Add the hovered to the current circle
+            d3.select(event.currentTarget)
+                .transition().duration(100)
+                .style("opacity", 1) 
+                .attr('stroke-width', 0.6)  // Add this line for the boundary width
+                .attr('r', 10);
+
             tooltip.transition()
                 .duration(100)
                 .style("opacity", .9);
-            tooltip.html(d[2])
+            tooltip.html("<b>Keywords:</b> "+ d[3].slice(0,10))
+                .style("font-size", "15px")
                 .style("left", (event.pageX + 5) + "px")
                 .style("top", (event.pageY - 28) + "px");
+
+            chatcontent.transition()
+                .duration(100)
+                .style("opacity", 1);
+            chatcontent.html(d[2])
         })
-        .on("mouseout", (d) => {
+        .on("mouseout", (d) => {            
             tooltip.transition()
                 .duration(500)
                 .style("opacity", 0);
         });
 
+    
+
+    // Render the legend
+    var keys = Object.keys(color_dict)
+    const legend = d3.select(legendref.current);
+    legend.selectAll('*').remove()
+
+
+    legend.selectAll('circle')
+        .data(keys)
+        .enter()
+        .append('circle')
+        .attr('cx', (d,i) => 10)
+        .attr('cy', (d,i) => 18+ 30*i)
+        .attr('r', 7)
+        .attr('fill', d => color_dict[d])
+        .attr('stroke', 'black')  // Add this line for the boundary color
+        .attr('stroke-width', 0.5)  // Add this line for the boundary width
+
+    legend.selectAll("mylabels").data(keys)
+        .enter()
+        .append("text")
+            .attr("x", 20)
+            .attr("y", function(d,i){ return 20 + i*30}) // 100 is where the first dot appears. 25 is the distance between dots
+            .style("fill",'black')
+            .text(function(d){ return d})
+            .attr("text-anchor", "left")
+            .style("alignment-baseline", "middle")
 
     // Bind data to text elements and add labels
-    svg.selectAll('text.label')
+    /*svg.selectAll('text.label')
         .data(labels)
         .enter()
         .append('text')
@@ -94,13 +152,35 @@ function Scatterplot(props) {
         .attr('stroke', 'white')  
         .attr('stroke-width', '0.3px') 
         .attr('font-size', '17px')
-        .attr('font-weight', '800') ;
+        .attr('font-weight', '800') ;*/
 
       
 
-  }, [data, labels, width, height]);
+  }, [data, labels, colorcol, width, height]);
 
-  return <svg ref={ref} width={width} height={height}></svg>;
+  return (
+  <>
+  <svg ref={ref} width={width} height={height}></svg>
+  <div style={{ position: 'absolute', top: "75%", left: '20px', width: '100px', backgroundColor: 'rgba(0, 0, 0, 0.1)',
+                 boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.5)',  // Drop shadow
+                borderRadius: '20px' ,                         // Curved edges
+                fontFamily: 'Perpetua',  // Setting the font family
+                padding: '15px',
+                overflow:'scroll',
+                textAlign:'left',
+                flex: 1
+                
+
+            }}>
+            <b>Legend</b>
+            
+
+            <svg ref={legendref}></svg>
+        </div>
+
+    </>
+
+  )
 }
 
 export default Scatterplot;
